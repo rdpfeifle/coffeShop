8a6549f2b3099a60a819cda04842595a0e92e019
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2022-10-13 13:16:13 -0600

    Completed lab

---
+++ static/coffeeShop.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const addButtons = document.querySelectorAll(".add-to-order");
+const totalCart = document.querySelector("#cart-total");
+
+for (const button of addButtons) {
+  button.addEventListener("click", () => {
+    const item = button.id;
+    let cart = {};
+    let orderTotal = 0;
+    fetch(`/update-cart.json?item=${item}`)
+      .then((response) => response.json())
+      .then((result) => {
+        cart = result.cart;
+        orderTotal = result.total;
+        displayCart(cart);
+        totalCart.innerHTML = `${orderTotal.toFixed(2)}`;
+      });
+  });
+}
+
+// Display the shopping cart items in the right-hand column of the page.
+function displayCart(cartContents) {
+  let tableContents = "";
+
+  // Object.entries is like Python's dict.items --- it's a nice way to
+  // loop over keys and values. Here's how this would look in Python:
+  //
+  //   for item, price in cart_contents.items():
+  //       # etc.
+  for (const [item, price] of Object.entries(cartContents)) {
+    tableContents += `<tr><td>${item}</td><td>${price}</td></tr>`;
+  }
+
+  document.querySelector("#cart-items").innerHTML = tableContents;
+}
+
+// Display the order total using a dollar sign and two decimal places.
+function displayOrderTotal(total) {
+  document.querySelector("#cart-total").innerHTML = `${total.toFixed(2)}`;
+}
